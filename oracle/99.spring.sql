56495d5ae57bc9af0e6203ddf301942aaa6172c9
wonku19 <133620204+wonku19@users.noreply.github.com>
2023-09-08 15:50:46 +0900

    0908-test

---
+++ oracle/99.spring.sql
@@ -0,0 +1,59 @@
+CREATE TABLE MEMBER(
+    ID VARCHAR2(100) PRIMARY KEY,
+    PASSWORD VARCHAR2(150) NOT NULL,
+    NAME VARCHAR2(50) NOT NULL,
+    ADDRESS VARCHAR2(200) NOT NULL
+);
+
+create table member(
+    id varchar2(50) primary key,
+    password varchar2(100) not null,
+    name varchar2(50) not null,
+    address varchar2(200),
+    auth varchar2(50) default 'ROLE_MEMBER' not null,
+    enabled number(1) default 1 not null
+);
+
+SELECT * FROM MEMBER;
+
+DROP TABLE MEMBER;
+-----------------------------------------------------------
+CREATE SEQUENCE SEQ_BOARD;
+CREATE TABLE BOARD(
+    NO NUMBER,
+    TITLE VARCHAR2(200) NOT NULL,
+    CONTENT VARCHAR2(2000) NOT NULL,
+    WRITER VARCHAR2(50) NOT NULL,
+    REGDATE DATE DEFAULT SYSDATE
+);
+
+ALTER TABLE board ADD url VARCHAR(200);
+
+SELECT * FROM BOARD;
+-----------------------------------------------------------
+create table company (
+	vcode varchar2(10) primary key,
+	vendor  varchar2(20) not null
+);
+insert  into company values('10', '삼성');
+insert  into company values('20', '애플');
+
+create table Phone(
+	num varchar2(10) primary key,
+	model varchar2(20) not null,
+	price number not null,
+	vcode varchar2(10),
+   constraint fk_vcode foreign key(vcode) references company(vcode)
+);
+insert into Phone values('ZF01','Galaxy Z Flip5', 1369000,'10');
+insert into Phone values('S918N','Galaxy S23 Ultra', 1479000,'10');
+insert into Phone values('IPO02','iPhone 14',1250000,'20');
+
+create table userinfo (
+	id varchar(20) primary key,
+	pw varchar(20) not null
+);
+insert into userinfo values('member','member');
+insert into userinfo values('admin','admin');
+commit;
+
